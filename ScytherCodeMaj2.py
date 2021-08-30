79fe0c3092006cbf7ab96c08e57279256592bf29
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-08-30 23:17:38 +0530

    Update ScytherCodeMaj2.py

--- ScytherCodeMaj2.py
+++ ScytherCodeMaj2.py
@@ -1,4 +1,4 @@
-protocol Major2(User,Cloud) 
+protocol Major2(User,CloudServer) 
 { 
 role User
 { 
@@ -7,8 +7,8 @@ var URi,CRi: Nonce;
 var URi'':Nonce;
 var RANcs,SKu-cs:Nonce; 
 macro Updi = H(Pdi, Rui); 
-send_!1(User,Cloud, IDi,Updi,UCi);
-recv_!2(Cloud,User,URi,UAi,CRi); 
+send_!1(User,CloudServer, IDi,Updi,UCi);
+recv_!2(CloudServer,User,URi,UAi,CRi); 
 macro UDi= XOR(URi,Pdi);
 macro UEi= XOR(CRi, H(IDi,Pdi));
 macro UFi= XOR(Rui, H(XOR(IDi, Pdi,UCi))); 
@@ -23,8 +23,8 @@ macro CRi'= XOR(UEi, H(IDi',Pdi'));
 macro UKi= XOR(UAi,H(IDi',Updi',CRi')); 
 macro ULi= H(IDi,UKi,CRi',RAN,Tui,URi'); 
 macro UNi= XOR(RAN, H(IDcs,URi'));
-send_!3(User,Cloud,IDi,UCi,ULi,UNi,Tui); 
-recv_!4(Cloud,User, UQ,UP,UW,Tcs); 
+send_!3(User,CloudServer,IDi,UCi,ULi,UNi,Tui); 
+recv_!4(CloudServer,User, UQ,UP,UW,Tcs); 
 macro SKu-cs''=XOR(UQ,H(IDi,URi,RAN)); 
 macro RANcs''= XOR(UW,RAN);
 macro RANcs'= {UP}SKu-cs''; 
@@ -37,17 +37,17 @@ claim(User,Secret,Pdi);
 claim(User,Secret,SKu-cs''); 
 }
 
-role Cloud 
+role CloudServer 
 {
 const Sc,UCi,CRi; 
 const IDcs; 
 var Rui,UCi: Nonce; 
 var RAN:Nonce; 
-recv_!1(User, Cloud, IDi,Updi,UCi); 
+recv_!1(User, CloudServer, IDi,Updi,UCi); 
 fresh URi,CRi: Nonce; 
 macro UAi= XOR(H(IDcs, SN, UCi), H(IDi,Updi,CRi)); 
-send_!2(Cloud,User,URi,UAi,CRi); 
-recv_!3(User,Cloud,IDi,UCi,ULi,UNi,Tui); 
+send_!2(CloudServer,User,URi,UAi,CRi); 
+recv_!3(User,CloudServer,IDi,UCi,ULi,UNi,Tui); 
 fresh URi'':Nonce; 
 macro RAN''= XOR(UNi, H(IDcs,URi''));
 macro UKi''= H(IDcs, SN, UCi); 
@@ -56,10 +56,10 @@ fresh RANcs,SKu-cs:Nonce;
 macro UQ= XOR(SKu-cs, H(IDi,URi,RAN''));
 macro UP= {RANcs}SKu-cs; 
 macro UW= XOR(RANcs,RAN''); 
-send_!4(Cloud,User, UQ,UP,UW,Tcs); 
-claim(Cloud,Niagree); 
-claim(Cloud,Nisynch); 
-claim(Cloud,Secret,RANcs);
-claim(Cloud,Secret,SKu-cs); 
+send_!4(CloudServer,User, UQ,UP,UW,Tcs); 
+claim(CloudServer,Niagree); 
+claim(CloudServer,Nisynch); 
+claim(CloudServer,Secret,RANcs);
+claim(CloudServer,Secret,SKu-cs); 
 }
 }
