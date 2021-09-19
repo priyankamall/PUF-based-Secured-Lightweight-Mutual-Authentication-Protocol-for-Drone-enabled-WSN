132f74d9e523e5996f7c7fa6ef21a15bc3944b57
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-19 10:09:19 +0530

    Update ScytherCodeMaj2.py

--- ScytherCodeMaj2.py
+++ ScytherCodeMaj2.py
@@ -12,7 +12,7 @@ RANcs,Tui ;
 const ADD: Function;
 protocol Major2(User,CloudServer) #authentication procedure between the User and the Cloud Server
 { 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5    
 role User
 { 
 fresh Rui,UCi: Nonce;#UCi Challenges
