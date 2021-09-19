1da34cacd585392fb4e67ce8b3965330bdc61890
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-19 10:06:54 +0530

    Update ScytherCodeMaj2.py

--- ScytherCodeMaj2.py
+++ ScytherCodeMaj2.py
@@ -5,6 +5,7 @@ const XOR:Function;
 const ADD:Function;
 const MUL:Function;
 const GEN:Function;
+  
 const IDi, Pdi, Updi, Tcs,UAi, IDcs, SN, Udi, UEi, UFi,
 UGi, IDi', Pdi', Rui', UGi', URi', Updi',CRi', UKi, ULi,
 UNi, RAN'', UKi'',ULi'', UQ, UP, UW, SKu-cs'', RANcs'',
